413629f8decea3b608e44ebd3ef0ba6a829f90ab
Artikon90 <artikon90@gmail.com>
2022-11-02 00:54:57 +0300

    /FLC-5/: Стартовый проект

---
+++ src/test/java/com/artikon90/flashcards/LanguageFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artikon90.flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageFlashcardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
